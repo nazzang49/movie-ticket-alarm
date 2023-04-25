1d6214decf6096e29a120f8a64ddfb51add5646a
nazzang49 <nazzang49@naver.com>
2023-04-26 00:24:30 +0900

    update file : success getting week no

--- src/test.py
+++ src/test.py
@@ -7,19 +7,14 @@ def get_week_no(y, m, d):
     firstday = input_date.replace(day=1)
     weekday = firstday.isoweekday() % 7
 
-    # 1일 = 일
     if weekday == 0:
         first_sunday = firstday
-    # 1일 = 월 ~ 수
-    elif weekday < 4:
-        first_sunday = firstday - timedelta(days=weekday + 1)
-    # 1일 = 목 ~ 토
+    elif weekday < 7:
+        first_sunday = firstday + timedelta(days=7 - weekday)
     else:
-        first_sunday = firstday + timedelta(days=6 - weekday)
+        raise ValueError("[OUT_OF_RANGE]")
 
     # +1 = consider <tr>
     return (input_date - first_sunday).days // 7 + 1
 
-print(get_week_no(2023, 5, 1))
-# print(now.isoweekday() % 7)
-# print(datetime.strptime(f"2023-04-01", "%Y-%m-%d"))
+print(get_week_no(2023, 4, 29))
